48f65a67551e1514612fab9bb1ad7a21df89431e
cuixiaqing <cuixiaqing@163.com>
2022-05-31 14:45:46 +0800

    init

---
+++ ActSpringDemo/src/test/java/com/roy/test/ActSpringTest.java
@@ -0,0 +1,25 @@
+package com.roy.test;
+
+import org.activiti.engine.RepositoryService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author ：楼兰
+ * @date ：Created in 2021/4/8
+ * @description:
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:activiti-spring.xml")
+public class ActSpringTest {
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Test
+    public void testRep(){
+        System.out.println(repositoryService);
+    }
+}
